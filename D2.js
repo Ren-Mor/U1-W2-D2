91c6284187dbf3533846cb887854bb86f1c5feaf
Ren-Mor <renatomorelli@hotmail.it>
2025-03-18 17:40:22 +0100

    added changes

--- D2.js
+++ D2.js
@@ -137,7 +137,9 @@ me.skills.pop();
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+const lista = [];
+lista.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(lista);
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
